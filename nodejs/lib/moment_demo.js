7938df20f72180895f56754e3968788c961244e0
Eric Zong <ericzonglu@126.com>
2019-07-03 18:06:46 +0800

    fix

--- nodejs/lib/moment_demo.js
+++ nodejs/lib/moment_demo.js
@@ -1,14 +1,13 @@
 var moment = require('moment');
 
 //moment.locale('zh-cn');
-var now = moment();
+//var now = moment();
 //console.log(now.format('YYYYMMDDHHmmssSSSS'));
-console.log(now.format('DD MMM YYYY'));
+//console.log(now.format('DD MMM YYYY'));
 
-//const getDateByNow = (offsetDays, dateFormat = 'YYYY-MM-DD') => {
-//    return moment().add(offsetDays).format(dateFormat);
-//};
-//
-//console.log(getDateByNow(15));
+const getDateByNow = (offsetDays, dateFormat = 'YYYY-MM-DD') => {
+    return moment().add(offsetDays).format(dateFormat);
+};
+console.log(getDateByNow(15));
 //console.log(getDateByNow(15, 'YYYYMMDD'));
 
