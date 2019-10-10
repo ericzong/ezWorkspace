3c218ad63af143f94547a1f67d2a5a08841abf23
Eric Zong <ericzonglu@126.com>
2019-10-10 18:34:35 +0800

    test locale

--- nodejs/lib/moment_demo.js
+++ nodejs/lib/moment_demo.js
@@ -4,10 +4,12 @@ var moment = require('moment');
 //var now = moment();
 //console.log(now.format('YYYYMMDDHHmmssSSSS'));
 //console.log(now.format('DD MMM YYYY'));
+moment.locale('zh-hk');
 
 const getDateByNow = (offsetDays, dateFormat = 'YYYY-MM-DD') => {
     return moment().add(offsetDays).format(dateFormat);
 };
 console.log(getDateByNow(15));
+console.log(getDateByNow(-15, 'DD MMM YYYY'));
 //console.log(getDateByNow(15, 'YYYYMMDD'));
 
